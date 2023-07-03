4abaaa3e472181468ca1fc8ee11b6abf4b38f1c2
Angst <sinxtra69@gmail.com>
2023-07-03 19:18:47 +0000

    added search button svg

--- client/src/routes/mock/+page.svelte
+++ client/src/routes/mock/+page.svelte
@@ -1,6 +1,12 @@
 <script>
+  import { SearchIcon } from 'lucide-svelte';
 </script>
 
 <div class="flex justify-center">
-	<input class="border rounded-xl px-4 py-1" type="text" value="sex" />
+  <div class="relative">
+    <button class="absolute left-1 top-1/2 transform -translate-y-1/2">
+          <SearchIcon class=" text-gray-500" />
+    </button>
+    <input class="border rounded-xl px-1 py-1 pl-10" type="text" placeholder="recipe" />
+  </div>
 </div>
