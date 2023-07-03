41119580043f51dbac4fdf74d25057f54196bd77
Angst <sinxtra69@gmail.com>
2023-07-03 02:52:27 +0000

    add basic api at api/search/query

--- recipes/api/index.py
+++ recipes/api/index.py
@@ -1,7 +1,21 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+load_dotenv()
+
+NINJA_API_KEY = os.getenv("NINJA_API_KEY")
+if not NINJA_API_KEY:
+    raise EnvironmentError("NINJA_API_KEY environment variable is not set.")
+
+@app.get("/api/search/{query}")
+def recipe(query: str):
+    # Make a request to the Ninja API with the API key to get random recipes
+    url = f"https://api.api-ninjas.com/v1/recipe?query={query}"
+    headers = {"X-Api-Key": NINJA_API_KEY}
+    resp = requests.get(url, headers={'X-Api-Key': NINJA_API_KEY}).json()
+    return {"recipes":resp}
+
