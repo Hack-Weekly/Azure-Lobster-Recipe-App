c875d1abeabf33af66b2918410535049d0d8ab4c
Angst <sinxtra69@gmail.com>
2023-07-03 21:57:59 +0000

    formatting

--- client/src/routes/mock/navbar.svelte
+++ client/src/routes/mock/navbar.svelte
@@ -1,14 +1,12 @@
 <script>
-	export let links = [
-		{ url: './mock', text: 'subpage1' },
-	];;
-  import { ChefHat } from 'lucide-svelte';
+	export let links = [{ url: './mock', text: 'subpage1' }];
+	import { ChefHat } from 'lucide-svelte';
 </script>
 
 <nav class="bg-black text-white mb-2">
 	<div class="container mx-auto px-4 py-2 flex justify-between items-center">
 		<div class="flex items-center">
-			<ChefHat class="h-8 w-8 mr-2"/>
+			<ChefHat class="h-8 w-8 mr-2" />
 			<span class="font-bold text-lg">Recipes</span>
 		</div>
 		<!-- Nav Links -->
