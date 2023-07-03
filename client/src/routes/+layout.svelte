62bb56edf04a542bcbc1596ea2c285ebb6db739c
Angst <sinxtra69@gmail.com>
2023-07-03 18:25:21 +0000

    added description/title to head

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import "../globals.css";
 </script>
-
+<svelte:head>
+	<title>Recipe App</title>
+	<meta name="description" content="Azure Lobster Recipe App" />
+</svelte:head>
 <slot />
